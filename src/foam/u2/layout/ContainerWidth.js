b36b390d1611c55dddc33353f695c1a4ce659c91
sarthak <81172969+sarthak-marwaha@users.noreply.github.com>
2024-10-29 13:06:39 -0400

    Fix resize observer errors (#3547)

--- src/foam/u2/layout/ContainerWidth.js
+++ src/foam/u2/layout/ContainerWidth.js
@@ -33,14 +33,18 @@ foam.CLASS({
   ],
 
   listeners: [
-    function updateWidth(entries) {
-      if ( entries.length > 1 ) console.warn('ContainerWidth called with mutiple observe targets');
-      for ( const entry of entries ) {
-        this.inlineSize = entry.target.getBoundingClientRect().width;
-        this.containerWidth = foam.u2.layout.DisplayWidth.VALUES
-        .concat()
-        .sort((a, b) => b.minWidth - a.minWidth)
-        .find(o => o.minWidth <= Math.min(this.inlineSize) );
+    {
+      name: 'updateWidth',
+      isFramed: true,
+      code: function updateWidth(entries) {
+        if ( entries.length > 1 ) console.warn('ContainerWidth called with mutiple observe targets');
+        for ( const entry of entries ) {
+          this.inlineSize = entry.target.getBoundingClientRect().width;
+          this.containerWidth = foam.u2.layout.DisplayWidth.VALUES
+          .concat()
+          .sort((a, b) => b.minWidth - a.minWidth)
+          .find(o => o.minWidth <= Math.min(this.inlineSize) );
+        }
       }
     }
   ]
