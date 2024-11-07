82467515c0f04414f038f7cd9a6bbd0550bd6b51
sarthak <sarthak@nanopay.net>
2024-11-07 09:59:10 -0500

    Fix duplicate validation messages

--- src/foam/nanos/auth/resetPassword/ResetPasswordByCode.js
+++ src/foam/nanos/auth/resetPassword/ResetPasswordByCode.js
@@ -31,13 +31,15 @@ foam.CLASS({
 
   sections: [
     {
-      name: 'verificationCodeSection'
+      name: 'verificationCodeSection',
+      title: ''
     },
     {
       name: 'resetPasswordSection'
     },
     {
       name: 'resetPasswordWizardSection',
+      title: '',
       properties: [ 'newPassword', 'confirmationPassword' ]
     }
   ],
@@ -59,7 +61,7 @@ foam.CLASS({
       padding: 0;
     }
     
-    .foam-u2-dialog-ApplicationPopup-bodyWrapper .subTitle {
+     .foam-u2-detail-SectionView-verificationCodeSection .subTitle, .foam-u2-detail-SectionView-verificationCodeSection .foam-u2-detail-SectionView-section-title {
       text-align: center;
     }
     .foam-u2-dialog-ApplicationPopup-bodyWrapper .foam-u2-detail-SectionView-verificationCodeSection {
