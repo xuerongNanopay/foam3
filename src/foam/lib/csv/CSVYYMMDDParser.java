c32313a209ce56804e2a88e9a811862071133b22
Mykola <mykola@nanopay.net>
2024-12-10 08:54:24 -0500

    add date parser

---
+++ src/foam/lib/csv/CSVYYMMDDParser.java
@@ -0,0 +1,80 @@
+/**
+ * @license
+ * Copyright 2024 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package foam.lib.csv;
+
+import foam.lib.parse.*;
+import foam.util.SafetyUtil;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
+import foam.lib.json.*;
+
+public class CSVYYMMDDParser
+  extends ProxyParser
+{
+  private final static Parser instance__ = new CSVYYMMDDParser();
+
+  public static Parser instance() { return instance__; }
+
+  protected static ThreadLocal<StringBuilder> sb = new ThreadLocal<StringBuilder>() {
+    @Override
+    protected StringBuilder initialValue() {
+      return new StringBuilder();
+    }
+
+    @Override
+    public StringBuilder get() {
+      StringBuilder b = super.get();
+      b.setLength(0);
+      return b;
+    }
+  };
+
+  public CSVYYMMDDParser() {
+    super(new Alt(
+      NullParser.instance(),
+      new Seq(
+        IntParser.instance(),
+        Literal.create("-"),
+        IntParser.instance(),
+        Literal.create("-"),
+        IntParser.instance()),
+      new LongParser()
+    ));
+  }
+
+  public PStream parse(PStream ps, ParserContext x) {
+    ps = super.parse(ps, x);
+
+    if ( ps == null ) return null;
+
+    if ( ps.value() == null ) return ps.setValue(null);
+
+    // Checks if Long Date (Timestamp from epoch)
+    if ( ps.value() instanceof Long ) {
+      return ps.setValue(new Date((Long) ps.value()));
+    }
+
+    Object[] result = (Object[]) ps.value();
+
+    Calendar c = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+    c.clear();
+
+    c.set(
+      (Integer) result[0],
+      (Integer) result[2] - 1, // Java calendar uses zero-indexed months
+      (Integer) result[4]
+    );
+
+    c.set(Calendar.DAY_OF_MONTH, (Integer) result[4]);
+    c.set(Calendar.HOUR_OF_DAY, 0);
+    c.set(Calendar.MINUTE, 0);
+    c.set(Calendar.SECOND, 0);
+
+    return ps.setValue(c.getTime());
+  }
+}
