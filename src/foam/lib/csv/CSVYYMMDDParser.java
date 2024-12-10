ab962809794c9e7f8a2a59efd01dcfc660368bcb
Mykola <mykola@nanopay.net>
2024-12-10 08:58:17 -0500

    remove extra

--- src/foam/lib/csv/CSVYYMMDDParser.java
+++ src/foam/lib/csv/CSVYYMMDDParser.java
@@ -70,11 +70,6 @@ public class CSVYYMMDDParser
       (Integer) result[4]
     );
 
-    c.set(Calendar.DAY_OF_MONTH, (Integer) result[4]);
-    c.set(Calendar.HOUR_OF_DAY, 0);
-    c.set(Calendar.MINUTE, 0);
-    c.set(Calendar.SECOND, 0);
-
     return ps.setValue(c.getTime());
   }
 }
