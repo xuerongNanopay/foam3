4d798aef0a8d70e088ddda198674a30230763621
Kevin Greer <kgrgreer@gmail.com>
2024-12-13 15:31:25 -0500

    Remove StatusWas, which isn't used.

--- src/foam/nanos/pom.js
+++ src/foam/nanos/pom.js
@@ -694,7 +694,6 @@ foam.POM({
     { name: "crunch/predicate/CapabilityIsStatus",                                        flags: "js|java" },
     { name: "crunch/predicate/CapabilityPrerequisitesGranted",                            flags: "js|java" },
     { name: "crunch/predicate/StatusChangedTo",                                           flags: "js|java" },
-    { name: "crunch/predicate/StatusWas",                                                 flags: "js|java" },
     { name: "crunch/predicate/DataChanged",                                               flags: "js|java" },
     { name: "crunch/predicate/IsAgent",                                                   flags: "js|java" },
     { name: "crunch/predicate/IsSpid",                                                    flags: "js|java" },
