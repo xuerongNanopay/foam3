f3957c76cb54cb8b57f4aba0e2300dc487e53024
Kevin Greer <kgrgreer@gmail.com>
2024-12-19 21:15:42 -0500

    Remove VerticalMenu from ZAC HelloWorld client nspec.

--- deployment/demo/src/foamdev/demo/zac/HelloWorld.js
+++ deployment/demo/src/foamdev/demo/zac/HelloWorld.js
@@ -27,19 +27,20 @@ foam.CLASS({
       this.SUPER();
 
       try {
-        if ( foam.nanos.zac.Client.isInstance(this.ctrl) )
-        this.ctrl.add(this);
+        if ( foam.nanos.zac.Client.isInstance(this.ctrl) ) {
+          this.ctrl.add(this);
+        }
       } catch (x) {}
     },
 
-    async function render() {
+    function render() {
       this.SUPER();
 
 //      await this.auth.authorizeAnonymous();
    // No need to log in as anon, since it is done automatically
    // var user = await this.auth.login(null, 'anon', '');
    // this.subject.user = this.subject.realUser = user;
-      this.add(this.VerticalMenu.create());
+      // this.add(this.VerticalMenu.create());
       this.add('Hello World!');
     }
   ]
