b44983dbe160f4d38451fd285f465006f5bd0a28
Kevin Greer <kgrgreer@gmail.com>
2024-12-29 10:10:20 -0500

    Improve layout of FLOW MarkupEditor.

--- src/foam/flow/MarkupEditor.js
+++ src/foam/flow/MarkupEditor.js
@@ -8,17 +8,14 @@ foam.CLASS({
   package: 'foam.flow',
   name: 'MarkupEditor',
   extends: 'foam.u2.View',
+
   requires: [
     // We don't require Document as it causes a circular dependency
     // that the classloader can't deal with yet.
 //    'foam.flow.Document',
     'foam.u2.tag.TextArea'
   ],
-  properties: [
-    {
-      name: 'preview'
-    }
-  ],
+
   css: `
 ^{
 table-layout: fixed;
@@ -29,6 +26,12 @@ table-layout: fixed;
 ^right {
 }`,
 
+  properties: [
+    {
+      name: 'preview'
+    }
+  ],
+
   methods: [
     function render() {
       this.onDetach(this.data$.sub(this.updatePreview));
@@ -36,30 +39,34 @@ table-layout: fixed;
 
       this.
         start('table').
+        style({width: '90%'}).
         addClass(this.myClass()).
         start('tr').
         start('td').
-        attrs({ valign: 'top' }).
-        addClass(this.myClass('left')).
-        add(this.slot(function(preview) {
-          return preview ? preview.toE(null, this.__subSubContext__) : this.E();
-        })).
+          attrs({ valign: 'top', width: '50%' }).
+          addClass(this.myClass('left')).
+          tag(this.TextArea, { rows: 24, cols: 80, onKey: true, data$: this.data$ }).
         end('td').
         start('td').
-        attrs({ valign: 'top' }).
-        addClass(this.myClass('right')).
-        start(this.TextArea, { rows: 24, cols: 80, onKey: true, data$: this.data$ }).
+          attrs({ valign: 'top', width: '50%' }).
+          style({'word-break': 'break-word'}).
+          addClass(this.myClass('right')).
+          add(this.slot(function(preview) {
+            return preview ? preview.toE(null, this.__subSubContext__) : this.E();
+          })).
         end('td').
         end('tr').
         end('table');
     }
   ],
+
   listeners: [
     {
       name: 'updatePreview',
       isMerged: 1000,
       code: function() {
-        this.preview = this.__context__.lookup('foam.flow.Document').create({ markup: this.data });
+        this.preview = this.__context__.lookup('foam.flow.Document').create(
+          {markup: this.data});
       }
     }
   ]
