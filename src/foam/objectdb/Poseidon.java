0b76221a1e8e4e11079af17d03578956c97b11f8
xuerongNanopay <xuerong@nanopay.net>
2024-12-30 19:01:53 -0500

    add poseidon project

---
+++ src/foam/objectdb/Poseidon.java
@@ -0,0 +1,11 @@
+/**
+ * @license
+ * Copyright 2024 The FOAM Authors. All Rights Reserved.
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+public class Poseidon {
+  static void say() {
+    System.out.println("This is Poseidon");
+  }
+}
