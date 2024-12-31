ef2ea6f60985f01d6261e3ebdd3c9cc3ba5f4096
xuerongNanopay <xuerong@nanopay.net>
2024-12-30 19:16:19 -0500

    add test function

--- src/foam/objectdb/Poseidon.java
+++ src/foam/objectdb/Poseidon.java
@@ -3,9 +3,10 @@
  * Copyright 2024 The FOAM Authors. All Rights Reserved.
  * http://www.apache.org/licenses/LICENSE-2.0
  */
+package foam.poseidon;
 
 public class Poseidon {
-  static void say() {
+  static public void say() {
     System.out.println("This is Poseidon");
   }
 }
