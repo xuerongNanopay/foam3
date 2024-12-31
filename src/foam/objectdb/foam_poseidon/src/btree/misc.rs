b537e94254979e98cc68eec36133bcfd267d8c65
xuerongNanopay <xuerong@nanopay.net>
2024-12-31 12:58:30 -0500

    add min max micro

--- src/foam/objectdb/foam_poseidon/src/btree/misc.rs
+++ src/foam/objectdb/foam_poseidon/src/btree/misc.rs
@@ -1,4 +1,5 @@
 #![allow(unused)]
+
 static FP_KILOBYTE: u64 = 1024;
 static FP_MEGABYTE: u64 = 1048576;
 static FP_GIGABYTE: u64 = 1073741824;
@@ -9,13 +10,37 @@ static FP_EXABYTE:  u64 = 1152921504606846976;
 static FP_DAY:    u64 = 86400;
 static FP_MINITE: u64 = 60;
 
+#[macro_export]
+macro_rules! FP_MIN {
+    ($a:expr, $b:expr) => {
+        if $a < $b { $a } else { $b }
+    };
+}
+
+#[macro_export]
+macro_rules! FP_MAX {
+    ($a:expr, $b:expr) => {
+        if $a > $b { $a } else { $b }
+    };
+}
+
+#[macro_export]
+macro_rules! FP_CLAMP {
+    ($x:expr, $low:expr, $high:expr) => {
+        FP_MIN!(FP_MAX!($x, $low), $high)
+    };
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn btree() {
-        // let result = add_numbers(2, 2);
-        // assert_eq!(result, 4);
+    fn test_min_max_micro() {
+        assert_eq!(FP_MIN!(1, 2), 1);
+        assert_eq!(FP_MAX!(1, 2), 2);
+        assert_eq!(FP_CLAMP!(1, 3, 5), 3);
+        assert_eq!(FP_CLAMP!(4, 3, 5), 4);
+        assert_eq!(FP_CLAMP!(6, 3, 5), 5);
     }
 }
