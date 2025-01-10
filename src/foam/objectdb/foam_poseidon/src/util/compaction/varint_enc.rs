659be901d1861b088bceea5fc5b4ad270e06f710
xuerongNanopay <xuerong@nanopay.net>
2025-01-09 23:38:35 -0500

    add macro to assert size

--- src/foam/objectdb/foam_poseidon/src/util/compaction/varint_enc.rs
+++ src/foam/objectdb/foam_poseidon/src/util/compaction/varint_enc.rs
@@ -3,7 +3,18 @@
 
 use crate::{error::FP_ILLEGAL_ARGUMENT, types::FPResult};
 
-pub fn decode() -> FPResult<u64> {
+macro_rules! SIZE_AT_LEAST {
+    ($min:expr, $len: expr) => {
+        if $len != 0 && $len > $min {
+            return Err(FP_ILLEGAL_ARGUMENT)
+        }
+    }
+}
+
+pub fn decode(b: &[u8], maxlen: usize) -> FPResult<u64> {
+    SIZE_AT_LEAST!(1, maxlen);
+
+    
     Err(FP_ILLEGAL_ARGUMENT)
 }
 
