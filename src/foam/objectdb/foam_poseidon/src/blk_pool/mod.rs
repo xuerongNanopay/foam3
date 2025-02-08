2705d607e61780f495fc0ceae6719e6423ce6602
xuerongNanopay <xuerong@nanopay.net>
2025-02-07 21:01:56 -0500

    save change

--- src/foam/objectdb/foam_poseidon/src/blk_pool/mod.rs
+++ src/foam/objectdb/foam_poseidon/src/blk_pool/mod.rs
@@ -2,9 +2,24 @@
 
 use crate::{error::FP_NO_IMPL, internal::FPResult};
 
+#[derive(Default, Clone, Copy)]
+enum BlkpoolType {
+    #[default]
+    Unconfigured,
+    Dram,
+    Nvram
+}
+
+struct Blkpool {
+}
+
+struct BlkpoolItem {
+
+}
+
 /**
  * Read block.
- * 
+ * __wt_blkcache_read
  * 1. Checks if the block exists in memory
  * 2. Reads from the block manager if not found in cache
  * 3. Handles decryption (if the block is encrypted). TODO
@@ -12,18 +27,39 @@ use crate::{error::FP_NO_IMPL, internal::FPResult};
  * 5. Verifies the block if required.
  * 6. Stores the block in the block cache if applicable.
  */
-
 fn blkpool_read(
     addr: &[u8]
 ) -> FPResult<()> {
 
+    'read_blk: loop {
+        //MUST TODO
+        // Try read from pool.
+        match blkpool_read_pool(addr)? {
+            Some(todo) => { break 'read_blk; },
+            None => {},
+        };
+
+        blkpool_read_blk(addr);
+
+        break 'read_blk;
+    }
     Err(FP_NO_IMPL)
 }
 
 /**
  * Try read block from pool.
  */
-fn blkpool_pool_read(
+fn blkpool_read_pool(
+    addr: &[u8]
+) -> FPResult<Option<()>> {
+
+    Err(FP_NO_IMPL)
+}
+
+/**
+ * Read block form file.
+ */
+fn blkpool_read_blk(
     addr: &[u8]
 ) -> FPResult<Option<()>> {
 
