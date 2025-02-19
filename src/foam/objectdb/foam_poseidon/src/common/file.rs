720bf019c93a0a326e1674dcdd35c1d50ed1aae5
xuerongNanopay <xuerong@nanopay.net>
2025-02-19 18:50:24 -0500

    add file wrapper

--- src/foam/objectdb/foam_poseidon/src/common/file.rs
+++ src/foam/objectdb/foam_poseidon/src/common/file.rs
@@ -1,2 +1,24 @@
 #![allow(unused)]
 
+use std::{fmt, fs::File, ops::Range};
+
+use crate::internal::FPResult;
+
+use super::ArcBytes;
+
+pub(crate) trait FileHandle: 'static + Send + Sync + fmt::Debug {
+    fn read(&self, range: Range<u64>) -> FPResult<ArcBytes>;
+}
+
+struct FileWrapper {
+    file: File,
+    len: u64,
+}
+
+impl FileWrapper {
+    pub(crate) fn new(file: File) -> FPResult<Self> {
+        let fd = FP_IO_ERR_RET!(file.metadata());
+        let len = fd.len();
+        Ok(FileWrapper {file, len})
+    }
+}
